41d640fdae052c2ecce645ce66d651c7de82beee
VineethReddy <eternalbasic1@gmail.com>
2022-12-23 11:33:32 +0530

    Implementing Middleware install body-parser,cookie-parser,cors npm and trying to implement them

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
+
 const mongoose = require('mongoose');
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const cors = require("cors");
+
 mongoose.set('strictQuery', false);//not very much useful as of now
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true,
@@ -13,6 +17,10 @@ mongoose.connect(process.env.DATABASE,{
 
 
 const port = process.env.PORT||8000;
+
+app.use(bodyParser.json());//it Parse incoming request(/ , /admin , /cart  ) bodies in a middleware before your handlers, available under the req.body property. In req.body property we have everything like req.body.name ,req.body.email , req.body.password etc..
+app.use(cookieParser());// Handles the header and populate req.cookie. If we want to set something into the cookie or if we want to get something form the cookie we use cookie-parser.
+app.use(cors());  // Cross-Origin Resource Sharing (CORS) is a security feature implemented by web browsers that blocks web pages from making requests to a different domain than the one that served the web page. This is done to prevent malicious websites from making requests to your web browser on behalf of the user.
 
 app.listen(port,() => {
     console.log(`App is up and running at ${port}`);
