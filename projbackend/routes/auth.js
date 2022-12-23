a707b637f8d1a23091a4e07bb877d592fbc048dc
VineethReddy <eternalbasic1@gmail.com>
2022-12-23 17:48:53 +0530

    Installed express-validator to make some validation in route level

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,11 +1,18 @@
 // We generally use app.get or app.post, But we don't do that instead we use express router in this we use router.get or router.post
 
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const { check , validationResult } = require('express-validator');
 
 const {signout,signup} = require("../controllers/auth");
 
-router.post("/signup",signup)
+router.post("/signup",[
+    check("name","Name Should be atleast 3 characters").isLength({min:3}),
+    check("email","Email is Required").isEmail(),
+    check("password","Password should be atleast 3 char").isLength({min: 3}),
+    ],
+    signup
+)
 
 
 router.get("/signout" , signout)
