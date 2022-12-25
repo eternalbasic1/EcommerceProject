ba1fd655c314a27d6e4a14d7eb853f1489ab176a
VineethReddy <eternalbasic1@gmail.com>
2022-12-25 10:24:02 +0530

    installed jsonwebtoken and express-jwt to create a token using _id and sending cookies into browser, Everything is successfully validating

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,40 @@
 const User = require("../models/user");
 const { check , validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
+exports.signin = (req,res) => {
+    const {email,password} = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array()[0].msg});
+      }
+
+      User.findOne({email}, (err,user) => {
+        if(err || !user){ // Intial we kept only if(err) but we need if(err || !user) keep in mind might be useful sometimes
+            return res.status(400).json({
+                error:"USER email does not exists"
+            })
+        }
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and password is not correct"
+            })
+        }
+
+        //CREATE TOKEN
+        const token = jwt.sign({_id: user._id},process.env.SECRET);
+
+        //PUT TOKEN IN COOKIE
+        res.cookie("token",token,{expire : new Date() + 9999});
+
+        //SEND RESPONSE TO FRONTEND
+        const {_id,name,email,role} = user;
+        return res.json({token,user:{_id,name,email,role}});
+
+      })
+    
+}
 
 
 exports.signup = (req,res) => {
@@ -24,6 +58,8 @@ exports.signup = (req,res) => {
         });
     })
 }
+
+
 exports.signout = (req,res) => {
     res.json({
         message: "User Signout"
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const { check , validationResult } = require('express-validator');
 
-const {signout,signup} = require("../controllers/auth");
+const {signout,signup,signin} = require("../controllers/auth");
 
 router.post("/signup",[
     check("name","Name Should be atleast 3 characters").isLength({min:3}),
@@ -12,7 +12,14 @@ router.post("/signup",[
     check("password","Password should be atleast 3 char").isLength({min: 3}),
     ],
     signup
-)
+);
+
+router.post("/signin",[
+    check("email","Email is Required").isEmail(),
+    check("password","Password field is required").isLength({min: 1}),
+    ],
+    signin
+);
 
 
 router.get("/signout" , signout)
