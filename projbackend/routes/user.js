f48b2ea70c36d1565d5c2b254d73660d72cacf9f
VineethReddy <eternalbasic1@gmail.com>
2022-12-25 11:55:46 +0530

    Created router.param(userId,getUserById) and router.get(/user/:userId, isSignedIn, isAuthenticated, getUser) and rectified few bugs in let checker = req.profile && req.auth && req.profile._id == req.auth._id it is not strong equals it will be weak equals(==)

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const {getUserById, getUser,} = require("../controllers/user");
 const {isSignedIn,isAuthenticated,isAdmin} = require("../controllers/auth");
 
-router.param("userId",isSignedIn,isAuthenticated,getUserById);
+router.param("userId",getUserById);
 
-router.get("/user/:userId", getUser);
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
 module.exports = router;
