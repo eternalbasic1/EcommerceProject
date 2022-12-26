82b7eb7f381b8bd98438c77b325afbc404ee8afe
VineethReddy <eternalbasic1@gmail.com>
2022-12-26 12:15:56 +0530

    Added formidable lodash node modules which allows us to use form data. why form data? form data allows us to store photo. in this process created a route createProduct which uses formidable and lodash

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 exports.getProductById = (req,res,next,id) => {
     Product.findById(id)
@@ -13,3 +16,40 @@ exports.getProductById = (req,res,next,id) => {
         next();
     });
 };
+
+
+exports.createProduct = (req,res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req,(err,fields, file) => {
+        if(err){
+            return res.status(400).json({
+                error: "Problem with Image"
+            });
+        }
+
+        //TODO: restrictions on feild
+        let product = new Product(fields)
+
+        //handle file here
+        if(file.photo){
+            if(file.photo.size > 3000000){
+                return res.status(400).json({
+                    error: "File size is too big"
+                });
+            }
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type
+        }
+        product.save((err,product => {
+            if(err){
+                return res.status(400).json({
+                    error: "Saving tshirt in DB failed"
+                });
+            }
+            res.json(product);
+        }));
+    });
+
+}
