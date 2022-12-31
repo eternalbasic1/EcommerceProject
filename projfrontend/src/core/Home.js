abbd1f5d659f7dc9f4a28835cecfe89cb63e9025
VineethReddy <eternalbasic1@gmail.com>
2022-12-31 11:49:25 +0530

    Included Bootstrap CDN and FontAwesome CDN in index.html and also included Styles.css(custom file)

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../styles.css"
 
 export default function Home() {
   return (
     <div>
-        <h1>Hello Front End</h1>    
+        <h1 className='text-white'>Hello Front End</h1>    
     </div>
   )
 }
